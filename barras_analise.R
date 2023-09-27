0da5dab52bf468e812f57addc0a842ca03dcc2bd
arslabadack <arslabadack@gmail.com>
2023-09-27 18:52:40 -0300

    Últimos ajustes

--- barras_analise.R
+++ barras_analise.R
@@ -5,7 +5,7 @@ barras_analise <- function(data, selected_column) {
   # Cria o gráfico
   grafico <- ggplot(data_filtered, aes_string(x = "Critérios", y = selected_column)) +
     geom_bar(stat = "identity", fill = "#E56A5DFF", color = "black") +
-    labs(x = "Itens", y = "Valores",title = "Critérios") +
+    labs(x = NULL, y = NULL,title = NULL) +
     theme_classic() +  # Estilo minimalista
     theme(legend.position = "none", axis.text = element_text(size = 14), axis.title = element_text(size = 16),plot.title = element_text(hjust = 0.5))
 
